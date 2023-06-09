7c270b2542d713de63698ff74969bb2fa52669bd
semon saleeb <semonsaleeb@gmail.com>
2023-06-09 16:54:15 +0200

    services

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-
-
-<?php 
-
-
-=======
 <?php
 
 include_once 'routes/products.php';
->>>>>>> 2ce3802c90f4dbe11c433afa8460f3bb1d37e39d
 ?>
---
+++ frontend/src/app/services/users.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) { }
+
+  getYsers(){
+    return this.http.get(`http://localhost/cafeteria/backend/users`)
+  }
+
+  getUser(id:number){
+    return this.http.get(`http://localhost/cafeteria/backend/users/${id}`)
+  }
+
+  addUser(formData:any){
+    return this.http.post(`http://localhost/cafeteria/backend/users` , formData)
+  }
+
+  updateUser(id:number , formData:any){
+    return this.http.post(`http://localhost/cafeteria/backend/users/${id}` , formData)
+  }
+
+  deleteUser(id:number){
+    return this.http.delete(`http://localhost/cafeteria/backend/users/${id}`)
+  }
+
+}
