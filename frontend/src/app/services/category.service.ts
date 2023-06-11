3573470e0c89b2b7d05b872dd06c4f6d8630b4fe
m-saleh96 <scyllies@gmail.com>
2023-06-11 12:12:53 +0300

    full admin-category with crud operation

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -12,8 +12,17 @@ export class CategoryService {
     return this.http.get('http://localhost/cafeteria/backend/categories');
   }
 
+  createCategory(formData:any) {
+    return this.http.post('http://localhost/cafeteria/backend/categories' , formData );
+  }
 
+  updateCategory(formData:any , id:any) {
+  return this.http.put('http://localhost/cafeteria/backend/categories/'+id , formData);
+  }
 
+  deletecategory(id:number) {
+    return this.http.delete(('http://localhost/cafeteria/backend/category/'+id));
+  }
 
 
 
