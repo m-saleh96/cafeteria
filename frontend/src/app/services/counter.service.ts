26d2f51a2d8190139cb19b1a6f405f4c2c9a9e83
m-saleh96 <scyllies@gmail.com>
2023-06-13 18:20:48 +0300

    requests service and increese count of cart

--- frontend/src/app/services/counter.service.ts
+++ frontend/src/app/services/counter.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
 
-  constructor() { }
+  // for cart
+  private counter = new BehaviorSubject(0);
+  counterVal = this.counter.asObservable();
+
+  setCounter(newCounterVal:number){
+    this.counter.next(newCounterVal);
+  }
+
+  // for orders
+  private orderCounter = new BehaviorSubject(0);
+  orderValCounter = this.orderCounter.asObservable();
+
+  setCounterOrder(newCounterVal:number){
+    this.orderCounter.next(newCounterVal);
+  }
+
+
+
+
 }
