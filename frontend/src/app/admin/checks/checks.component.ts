122e05f3b72c9ea4586a4d5ea3a8b383d7885d43
semon saleeb <semonsaleeb@gmail.com>
2023-06-14 19:22:24 +0200

    checks v2

--- frontend/src/app/admin/checks/checks.component.ts
+++ frontend/src/app/admin/checks/checks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ChecksOrder } from 'src/app/interfaces/checks-order';
 import { Users } from 'src/app/interfaces/users';
 import { OrderService } from 'src/app/services/order.service';
@@ -10,21 +11,38 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./checks.component.css']
 })
 export class ChecksComponent {
-  users !:Users[];
-  checks !:ChecksOrder[];
+  users !: Users[];
+  checks !: ChecksOrder[];
 
-  constructor(private usersService:UsersService,private orderService: OrderService){}
-  
-  ngOnInit(){
-    // this.usersService.getUsers().subscribe((res: any) => {
-    //   this.users=res;
-    //   // console.log(this.users);
-    // });
+  constructor(private usersService: UsersService, private orderService: OrderService) { }
 
-    this.orderService.getOrders().subscribe((res:any)=>{
-      this.checks=res;
-console.log(this.checks);
+  ngOnInit() {
+    this.usersService.getUsers().subscribe((res: any) => {
+      this.users = res;
+      // console.log(this.users);
+    });
+
+    this.orderService.getOrders().subscribe((res: any) => {
+      this.checks = res;
+      console.log(this.checks);
 
     })
+  }
+  start_date: any = '';
+  end_Date: any = '';
+  user: any = '';
+
+
+  onSubmit(startDate: NgForm, end_Date: NgForm, user: NgForm) {
+    this.start_date = startDate;
+    console.log(this.start_date);
+    console.log(end_Date);
+    console.log(user);
+    let selectedMembers = this.checks.filter(m => {
+      if ( m.created_at > this.start_date &&  m.created_at< this.end_Date && m.name==this.user) // or you can cast here to Date()
+        console.log(m);
+});
+
+
   }
 }
